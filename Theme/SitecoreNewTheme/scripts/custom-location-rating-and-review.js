c3576d011b31be6e52d78d3f773e0dda4cebbfa3
guptasandeep <34857566+guptasandeep@users.noreply.github.com>
2023-10-31 10:28:44 +0530

    SXA Map component Part 7: Most Relevant Google reviews (#3)
    SXA Map component Part 7: Most Relevant Google reviews
    
    ---------
    
    Co-authored-by: Gupta Sandeepkumar <gupta.sandeepkumar@perficient.com>

--- Theme/SitecoreNewTheme/scripts/custom-location-rating-and-review.js
+++ Theme/SitecoreNewTheme/scripts/custom-location-rating-and-review.js
@@ -50,6 +50,52 @@ XA.component.customLocationRatingAndReview = (function ($, document) {
 						ratingAndReviewDiv.toggleClass('ratings-and-reviews');
 						ratingAndReviewDiv.removeClass('d-none');
 						ratingAndReviewDiv.html(ratingAndReviewDiv.html().replace('#average_rating#', `${place.rating.toFixed(1)} <i data-star="${place.rating.toFixed(1)}"></i>`).replace('#review_count#', place.user_ratings_total));
+						
+						//On click of Rating and review text, open the Modal showing the recent reviews.  
+						ratingAndReviewDiv.click(function(){
+							var existingReviewModal = $(`${ratingAndReviewDiv.attr('id')}-modal`);
+							var reviewModal;
+							var exampleModel = $('#exampleModal');
+							if(exampleModel.length == 0)
+							{
+								return;
+							}
+							
+							if(existingReviewModal.length == 0)
+							{
+								var locationName = $(`#${ratingAndReviewDiv.attr('id')}-main .title`).text();
+								var ratingAndReviewData = ratingAndReviewDiv.html();
+								reviewModal = exampleModel.clone();
+								reviewModal.attr('id',`${ratingAndReviewDiv.attr('id')}-modal`);
+								reviewModal.removeClass('d-none');
+								reviewModal.find('.modal-title').text(locationName);								
+								reviewModal.find('.modal-rating-and-review').html(ratingAndReviewData);
+								var commentTemplate = reviewModal.find('.google-review-comment');
+								$.each(place.reviews, function(i, comment) {
+									var reviewComment = commentTemplate.clone();
+									reviewComment.find('.profile_photo_url').attr('src',comment.profile_photo_url);
+									reviewComment.find('.author_name').text(comment.author_name);
+									reviewComment.find('.author_name').attr('href',comment.author_url);
+									reviewComment.find('.rating').html( `${comment.rating.toFixed(1)} <i data-star="${comment.rating.toFixed(1)}"></i>`);
+									reviewComment.find('.relative_time_description').text(comment.relative_time_description);
+									reviewComment.find('.time').text(getDateAndTimeFromTimestamp(comment.time));
+									reviewComment.find('.text').text(comment.text);
+									reviewComment.appendTo(reviewModal.find('.google-review-comment').parent());
+									reviewComment.removeClass('d-none');
+								});
+								reviewModal.find('.more_reviews').attr("href",`https://www.google.com/maps?q=${encodeURIComponent(locationName)}`);
+								reviewModal.find('.more_reviews').detach().appendTo(reviewModal.find('.google-review-comment').parent());
+								reviewModal.appendTo($('exampleModal').parent());
+							}
+							else
+							{
+								reviewModal = existingReviewModal;
+							}
+							var myModal = new bootstrap.Modal(reviewModal, {
+							  keyboard: false
+							});
+							myModal.toggle();
+						});
 						noRatingAndReviewDiv.remove();
 					}
 				}
@@ -71,6 +117,13 @@ XA.component.customLocationRatingAndReview = (function ($, document) {
 				});				
 			}
 		});
+	}
+	
+	function getDateAndTimeFromTimestamp(timestamp)
+	{
+		const date = new Date(timestamp * 1000);
+		const formattedDate = date.toLocaleString();
+		return formattedDate;
 	}
 		
     return api;
